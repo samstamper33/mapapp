2721431b6690fa23aec0ee7295bc26f6e59642a5
samstamper33 <120147816+samstamper33@users.noreply.github.com>
2022-12-09 11:44:13 -0600

    Update poi.dart

--- lib/poi.dart
+++ lib/poi.dart
@@ -4,46 +4,90 @@
 
 import 'dart:convert';
 
-List<PointOfInterest> pointOfInterestFromJson(String str) => List<PointOfInterest>.from(json.decode(str).map((x) => PointOfInterest.fromJson(x)));
+PointOfInterest pointOfInterestFromJson(String str) => PointOfInterest.fromJson(json.decode(str));
 
-String pointOfInterestToJson(List<PointOfInterest> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String pointOfInterestToJson(PointOfInterest data) => json.encode(data.toJson());
 
 class PointOfInterest {
     PointOfInterest({
+        required this.response,
+    });
+
+    Response response;
+
+    factory PointOfInterest.fromJson(Map<String, dynamic> json) => PointOfInterest(
+        response: Response.fromJson(json["response"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "response": response.toJson(),
+    };
+}
+
+class Response {
+    Response({
+        this.cursor,
+        this.results,
+        this.count,
+        this.remaining,
+    });
+
+    int? cursor;
+    List<Result>? results;
+    int? count;
+    int? remaining;
+
+    factory Response.fromJson(Map<String, dynamic> json) => Response(
+        cursor: json["cursor"],
+        results: List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+        count: json["count"],
+        remaining: json["remaining"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "cursor": cursor,
+        "results": List<dynamic>.from(results!.map((x) => x.toJson())),
+        "count": count,
+        "remaining": remaining,
+    };
+}
+
+class Result {
+    Result({
         this.description,
-        required this.name,
-        required this.mainImage,
-        required this.createdDate,
+        this.name,
+        this.mainImage,
+        this.createdDate,
         this.createdBy,
         this.modifiedDate,
-        required this.poiType,
-        required this.longitude,
-        required this.latitude,
-        required this.markerImage,
-        required this.id,
-        required this.attraction,
+        this.poiType,
+        this.longitude,
+        this.latitude,
+        this.markerImage,
+        this.id,
+        this.attraction,
         this.animalType,
         this.imageGallery,
         this.rideType,
     });
 
     String? description;
-    String name;
-    String mainImage;
+    String? name;
+    String? mainImage;
     DateTime? createdDate;
     String? createdBy;
     DateTime? modifiedDate;
-    String poiType;
-    double longitude;
-    double latitude;
-    String markerImage;
-    String id;
-    String attraction;
+    String? poiType;
+    double? longitude;
+    double? latitude;
+    String? markerImage;
+    String? id;
+    String? attraction;
     String? animalType;
     List<String>? imageGallery;
     String? rideType;
 
-    factory PointOfInterest.fromJson(Map<String, dynamic> json) => PointOfInterest(
+    factory Result.fromJson(Map<String, dynamic> json) => Result(
         description: json["Description"] == null ? null : json["Description"],
         name: json["Name"] == null ? null : json["Name"],
         mainImage: json["Main Image"] == null ? null : json["Main Image"],
